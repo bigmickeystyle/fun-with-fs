9ec012835585edda770120cca55f48ef1669b971
bigmickeystyle <mike.123.smith@gmail.com>
2016-09-27 13:07:14 +0200

    exercise 2 complete

---
+++ part2.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var filePath = './files';
+var files = fs.readdirSync(filePath);
+var obj = {};
+
+function getSize(path, object){
+    files.forEach(function(file){
+        var nextPath = path + '/' + file;
+        var stat = fs.statSync(nextPath);
+        if(!stat.isDirectory()){
+            object[file] = stat.size;
+        }
+        else if(stat.isDirectory()){
+            files = fs.readdirSync(nextPath);
+            object[file] = getSize(nextPath, {});
+        }
+        return object;
+    });
+    return object;
+}
+
+var fin = getSize(filePath, obj);
+
+console.log(JSON.stringify(fin, null, 4));
